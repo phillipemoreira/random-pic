098c3d655e95e28960bee9bc4fa3953bb312bdd3
Phillipe Moreira <phillipealexmoreira@gmail.com>
2019-11-23 14:53:16 -0300

    adding spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
   function changeImage() {
       var keyword = "monster";
 
+      document.getElementById('spinner-div').classList.remove('invisible');
+      document.getElementById('image-container').classList.add('withOverlay');
+
       $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
               tags: keyword,
               tagmode: "any",
@@ -11,9 +14,11 @@
               var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
               document.getElementById("the-image").src = image_src;
 
+              setTimeout(() => {
+                document.getElementById('spinner-div').classList.add('invisible');
+                document.getElementById('image-container').classList.remove('withOverlay');
+              }, 500);
           });
-
-      document.getElementById("the-image").src = "https://images-eu.ssl-images-amazon.com/images/I/71oIx-9zYXL.png";
   }
 
   document.getElementById("the-button").addEventListener("click", function () {
