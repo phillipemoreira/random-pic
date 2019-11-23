a76e242e7dbdd7e96e29f4c62817aecd9a66524f
Phillipe Moreira <phillipealexmoreira@gmail.com>
2019-11-23 13:14:37 -0300

    Sending HTMl

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    //res.send('Hello World!!');
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
   
 app.listen(3000, function () {
